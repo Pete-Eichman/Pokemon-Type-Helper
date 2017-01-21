afb9fa1bfec251752c072faacd035d1793008b65
Pete Eichman <peichman14@gmail.com>
2017-01-21 16:39:44 -0500

    fix

--- spec/features/01_user_homepage_view_spec.rb
+++ spec/features/01_user_homepage_view_spec.rb
@@ -6,6 +6,8 @@ feature "User visits the home page" do
       visit '/'
 
       expect(page).to have_content "Welcome to Pokemon Type Helper!"
+      expect(page).to have_link "Attack Types"
+      expect(page).to have_link "Defenders"
     end
   end
 end
